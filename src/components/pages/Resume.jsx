8d8132156d328d7f6c497d4709718affc53d14f3
Diego Arteaga <148501504+diego-arteaga0@users.noreply.github.com>
2026-02-14 13:18:23 -0800

    Updated resume

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,4 +1,4 @@
-import pdf from "../assets/Diego_Arteaga_Resume_2_13_26.pdf";
+import pdf from "../assets/Diego_Arteaga_Resume_2_14_26_analyst.pdf";
 import Header from "../Header";
 import Footer from "../Footer";
 
